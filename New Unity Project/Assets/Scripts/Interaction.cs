b43aa7606cacbd65a9cfd3849119d9d86fe983cc
161918 <32957559+161918@users.noreply.github.com>
2020-04-27 22:37:42 +0200

    Commandes Vr + scènes 2
    PLZ FONCTIONNE

---
+++ New Unity Project/Assets/Scripts/Interaction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Interaction : MonoBehaviour
+{
+
+    public Text textshowed = null;
+    public void changeWord (string word)
+    {
+        textshowed.text = word;
+    }
+}
---
+++ New Unity Project/Assets/Scripts/playerController1.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController1 : MonoBehaviour
+{
+    public float movSpeed = 5.0f;
+    public float mouseSensitivity = 1.0f;
+
+    float rotUpDown = 0;
+    public float upDownLimit = 60;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotations
+
+        // X
+
+        float rotLeftRight = Input.GetAxis("Mouse X") * mouseSensitivity;
+        transform.Rotate(0, rotLeftRight, 0);
+
+        // Y
+
+        rotUpDown -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        rotUpDown = Mathf.Clamp(rotUpDown, -upDownLimit, upDownLimit);
+        Camera.main.transform.localRotation = Quaternion.Euler(rotUpDown, 0, 0);
+
+        // Movements
+
+        float forwardSpeed = Input.GetAxis("Vertical") * movSpeed;
+        float sideSpeed = Input.GetAxis("Horizontal") * movSpeed;
+
+        Vector3 speed = new Vector3(sideSpeed, 0, forwardSpeed);
+
+        speed = transform.rotation * speed;
+
+        CharacterController cc = GetComponent<CharacterController>();
+
+        cc.SimpleMove( speed );
+    }
+}
