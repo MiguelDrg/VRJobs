f7c204a210c649a06ecdd2f0a481ce7dead9deb0
Skynigga <43567096+Skynigga@users.noreply.github.com>
2020-04-27 16:22:30 +0200

    Video360 + Infos

---
+++ New Unity Project/Assets/Scripts/video.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class video : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //On définit les bases
+        //Image Noir de Fade de scène
+        GameObject ImageFade = GameObject.Find("BlackImage");
+        CanvasGroup canvasGroup = ImageFade.GetComponent<CanvasGroup>();
+        //Message d'info1
+        GameObject infoOne = GameObject.Find("infoPane");
+        CanvasGroup canvasGroupInfoOne = infoOne.GetComponent<CanvasGroup>();
+        canvasGroupInfoOne.alpha = 0;
+        //Message d'info2
+        GameObject infoTwo = GameObject.Find("infoPane2");
+        CanvasGroup canvasGroupInfoTwo = infoTwo.GetComponent<CanvasGroup>();
+        canvasGroupInfoTwo.alpha = 0;
+
+        //On lance la scène
+        StartCoroutine(Routine(canvasGroupInfoOne, canvasGroupInfoTwo, canvasGroup));
+    }
+
+    private IEnumerator Routine(CanvasGroup canvasGroupInfoOne, CanvasGroup canvasGroupInfoTwo, CanvasGroup canvasGroup)
+    {
+        //On fade IN PUIS ON LANCE
+        StartCoroutine(FadeEffect.FadeCanvas(canvasGroup, 1, 0, 2));
+        GameObject video = GameObject.Find("360_VR Master Series _ Free Download _ London On Tower Bridge");
+        var videoPlayer = video.GetComponent<UnityEngine.Video.VideoPlayer>();
+        videoPlayer.Play();
+        //ON AFFICHE LES 1E INFOS
+        yield return new WaitForSeconds(2);
+        StartCoroutine(FadeEffect.FadeCanvas(canvasGroupInfoOne, 0, 1, 1));
+        yield return new WaitForSeconds(8);
+        StartCoroutine(FadeEffect.FadeCanvas(canvasGroupInfoOne, 1, 0, 1));
+        StartCoroutine(FadeEffect.FadeCanvas(canvasGroupInfoTwo, 0, 1, 1));
+        yield return new WaitForSeconds(8);
+        StartCoroutine(FadeEffect.FadeCanvas(canvasGroupInfoTwo, 1, 0, 1));
+        StartCoroutine(FadeEffect.FadeCanvas(canvasGroup, 0, 1, 2));
+        yield return new WaitForSeconds(2);
+        videoPlayer.Pause();
+
+    }
+}
